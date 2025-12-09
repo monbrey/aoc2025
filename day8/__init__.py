6df1121b8599f97edf9554667be5f7671b2df3e4
monbrey <monbrey@gmail.com>
2025-12-09 14:49:33 +1100

    fix: day 8a

--- day8/__init__.py
+++ day8/__init__.py
@@ -17,16 +17,18 @@ def a():
     
     junctions = [tuple(int(x) for x in point.split(",")) for point in data.splitlines()]
     circuits = [[j] for j in junctions]
+    # calculating all distances and sorting probably isn't most efficient
+    # j > i ensures we don't double-calculate reverse relationships
     dists = sorted([(p,q,euc_dist(p,q)) for i,p in enumerate(junctions) for j,q in enumerate(junctions) if j > i], key=lambda x: x[2])
 
-    for d in dists:
+    # Instructions say to make 1000 connections
+    for d in dists[:1000]:
         c1 = [c for c in circuits if d[0] in c][0]
         c2 = [c for c in circuits if d[1] in c][0]
         if(c1 == c2):
-            # print("Both points already in same circuit, skipping")
+            # Ignore if in same circuit
             continue
         else:
-            # print("Merging circuits",c1,"and",c2)
             circuits.remove(c1)
             circuits.remove(c2)
             circuits.append(c1 + c2)
